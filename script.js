1ee0c85b2c9f7a579fc2dda2f5d7bfe36fb3c420
Nick James <nickjamesridgway@gmail.com>
2021-09-22 14:56:17 -0500

    First upload

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+function generatePassword() {
+
+  //declare the arrays
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+//blank arrays
+var resultArray = [];
+var userArray = [];
+
+uppercaseArray [1]
+
+  var numCharacter = prompt ("How many characters?");
+  var numbers = confirm ("Do you want numbers in your password?");
+  var uppercases = confirm ("Do you want uppercases letters in your password?");
+  var lowercases = confirm ("Do you want lowercases letters in your password?");
+  var characters = confirm ("Do you want special characters in your password?");
+
+
+//conditions
+if (numbers){
+  resultArray = resultArray.concat(numbArray); 
+}
+if (uppercases){
+  resultArray = resultArray.concat(uppercaseArray);
+}
+if (lowercases){
+  resultArray = resultArray.concat(lowercaseArray);
+}
+if (characters){
+  resultArray = resultArray.concat(characterArray);
+}
+console.log(resultArray)
+
+
+for (var i = 0; i < numCharacter; i++) {
+      
+  userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]); 
+  }
+  return userArray.join("") ;
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
+function copyToClipboard() {
+}
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
